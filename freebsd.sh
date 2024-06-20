4afdb6812a6b4f89790f21c3f6ccf52d31b61136
threepwood79 <cameron.taylor146@gmail.com>
2024-06-20 13:04:22 +0100

    Update freebsd.sh

--- freebsd.sh
+++ freebsd.sh
@@ -11,17 +11,30 @@ read -p "Please select an option (1-4): " choice
 case $choice in
 1)
     echo "Installing Gnome..."
+    pkg install -y xorg
     pkg install -y gnome
     ;;
 
 2)
     echo "Installing KDE..."
+    pkg install -y xorg
     pkg install -y  kde plasma5
     ;;
 
 3)
     echo "Installing i3wm..."
-    pkg install -y  i3 i3status i3lock dmenu i3-gaps
+    pkg install -y xorg
+    pkg install -y i3 i3status i3lock dmenu i3-gaps
+    pkg install -y ant-dracula-theme-2.0
+    pkg install -y lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+    pkg install -y lxappearance
+    pkg install -y compton compton-conf
+    pkg install -y arandr
+    pkg install -y volumeicon
+    pkg install -y alsa-utils
+    pkg install -y nitrogen
+    pkg install -y pcmanfm
+    pkg install -y lxmenu-data
     ;;
 
 4)
@@ -41,6 +54,7 @@ case $video_choice in
     echo "Installing video drivers..."
     pkg install -y drm-kmod
     pkg install -y nvidia-driver-470
+    sysrc moused_enable="YES" dbus_enable="YES" hald_enable="YES" ntpd_enable="YES" kld_list="/boot/modules/i915kms.ko" kld_list="i915kms" lightdm_enable="YES" kld_list="nvidia" kld_list="linux nvidia" kld_list="nvidia-modeset" kld_modset_name="nvidia-modeset"
     ;;
 [nN])
     echo "Skipping video driver installation."
